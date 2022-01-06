cd803e0e1294c5510f91b4caf558226d8de8af32
ShariqAteeq <m.shariq22@hotmail.com>
2022-01-06 22:25:18 +0500

    worked on place order

--- functions/place-order.js
+++ functions/place-order.js
@@ -1,14 +1,13 @@
 const AWS = require("aws-sdk");
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const ULID = require("ulid");
-const { getProductByID } = require("../lib/products");
 
 const { PRODUCTS_TABLE, ORDERS_TABLE } = process.env;
 
 module.exports.handler = async (event) => {
   const { username } = event.identity;
   const { orderItems, shippingId, shippingAddress, total, paymentId } =
-    event.arguments;
+    event.arguments.input;
   const orderId = ULID.ulid();
 
   let payload = {
@@ -28,33 +27,32 @@ module.exports.handler = async (event) => {
   }
 
   try {
-
-    const product = await getProductByID(productId);
-    if (!product) {
-      throw new Error("Product Not Found");
-    }
-
-    const transactItems = [
+    let transactItems = [
       {
         Put: {
           TableName: ORDERS_TABLE,
           Item: payload,
         },
       },
-      {
-        Update: {
-          TableName: PRODUCTS_TABLE,
-          Key: {
-            id: productId,
-          },
-          UpdateExpression: "SET reviews = :reviews",
-          ExpressionAttributeValues: {
-            ":reviews": reviews,
-          },
-          ConditionExpression: "attribute_exists(id)",
-        },
-      },
     ];
+
+    if (orderItems.length) {
+      for (const x of orderItems) {
+        transactItems.push({
+          Update: {
+            TableName: PRODUCTS_TABLE,
+            Key: {
+              id: x.productId,
+            },
+            UpdateExpression: "ADD quantity :minusOne",
+            ExpressionAttributeValues: {
+              ":minusOne": -x.quantity,
+            },
+            ConditionExpression: "attribute_exists(id)",
+          },
+        });
+      }
+    }
 
     await dynamodb.transactWrite({ TransactItems: transactItems }).promise();
     return payload;
